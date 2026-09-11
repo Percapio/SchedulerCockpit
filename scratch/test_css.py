1af3940aa01072a2215f3bd2ba81b44d25ffb8e2
Percapio <lincolnhedge@yahoo.com>
2026-09-11 07:50:54 -0700

    cleanup

--- scratch/test_css.py
+++
@@ -1,7 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QLabel
-app = QApplication(sys.argv)
-l = QLabel('Test')
-l.setStyleSheet('padding-left: 3%;')
-print(l.styleSheet())
-print(l.geometry())
